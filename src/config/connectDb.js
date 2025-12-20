ecef3421ed3c609da211d5b8a36a07e592820bfb
raaz-srijan <srijannospace@gmail.com>
2025-12-20 16:07:06 +0545

    package.json

--- src/config/connectDb.js
+++ src/config/connectDb.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 async function connectDb() {
     try {
         const connect = await mongoose.connect(process.env.MONGO_URI);
-        console.log(`Database connected successfully: ${connect.connection.host}`);
+        console.log(`Database connected successfully!`);
         return connect;
     } catch (error) {
         console.error("Database connection failed:", error.message);
